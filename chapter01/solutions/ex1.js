2aa732f0e5085aeeca84a5faf5a3a00b7e1e42a1
Thiago Perrotta <tbperrotta@gmail.com>
2025-05-24 05:36:18 -0400

    docs(chapter01, ex1): add more solutions

--- chapter01/solutions/ex1.js
+++ chapter01/solutions/ex1.js
@@ -2,8 +2,19 @@ import test from "node:test";
 import assert from "node:assert";
 
 function compile(code) {
-  //return [...(new TextEncoder).encode('\0asm'), ...[1, 0, 0, 0]];
-  //return [(new TextEncoder).encode('\0asm'), [1, 0, 0, 0]].flat(1);
+  // return [...(new TextEncoder).encode('\0asm'), ...[1, 0, 0, 0]];
+  // return [(new TextEncoder).encode('\0asm'), [1, 0, 0, 0]].flat(1);
+  // return [...[...(new TextEncoder).encode('\0asm')], 1, 0, 0, 0];
+  // return [
+  //   0x00, // '\0' (magic number)
+  //   0x61, // 'a'
+  //   0x73, // 's'
+  //   0x6d, // 'm'
+  //   0x01, // version
+  //   0x00, // version
+  //   0x00, // version
+  //   0x00, // version
+  // ];
   return [0, 97, 115, 109, 1, 0, 0, 0];
 }
 
