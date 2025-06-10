29b5648f40d6c0ffe6949876d21ec3c67006c23c
Mariano Guerra <mariano@marianoguerra.org>
2025-06-10 13:10:10 +0200

    ch01-ex1: improve solution

--- chapter01/ex1/solution.js
+++ chapter01/ex1/solution.js
@@ -1,5 +1,14 @@
 function compile(code) {
-  //return [...(new TextEncoder).encode('\0asm'), ...[1, 0, 0, 0]];
-  //return [(new TextEncoder).encode('\0asm'), [1, 0, 0, 0]].flat(1);
+  //return [...new TextEncoder().encode('\0asm'), ...[1, 0, 0, 0]];
+  //return [Array.from(new TextEncoder().encode('\0asm')), [1, 0, 0, 0]].flat(1);
   return [0, 97, 115, 109, 1, 0, 0, 0];
+}
+
+function int32le(v) {
+  return [
+    (v & 0xff) >>> 0,
+    (v & 0xff00) >>> 8,
+    (v & 0xff0000) >>> 16,
+    (v & 0xff000000) >>> 24,
+  ];
 }
