04b91cce4dc7243f75091b102965480884b8ad73
Mariano Guerra <mariano@marianoguerra.org>
2025-06-10 14:11:51 +0200

    fix solution

--- chapter01/ex5/solution.js
+++ chapter01/ex5/solution.js
@@ -1,25 +1,20 @@
+const SECTION_ID_EXPORT = 7;
 function exportsec(exports) {
-  return [
-    // ----- export section -----
-    7, // Section identifier
-    5, // Section size in bytes
-    1, // Number of entries that follow
-  ];
+  return w.section(SECTION_ID_EXPORT, w.vec(exports));
 }
 
 function export_(nm, exportdesc) {
   const name = w.stringToBytes(nm);
   return [
     // export section - entry 0
-    name.length, // Name size in bytes
+    u32(name.length), // Name size in bytes
     ...name,
-    0, // export type: function
     exportdesc, // function index
   ];
 }
 
 const exportdesc = {
   func(idx) {
-    return idx;
+    return [0x00, u32(idx)];
   },
 };
